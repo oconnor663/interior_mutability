e2e178fdccf879294dc0535e3c6ca1f6336e4900
Jack O'Connor <oconnor663@gmail.com>
2026-04-12 13:59:27 -0700

    don't use deref_mut()

--- examples/03_local_mutex_expanded.rs
+++ examples/03_local_mutex_expanded.rs
@@ -1,4 +1,3 @@
-use std::ops::DerefMut;
 use std::sync::{Mutex, MutexGuard};
 
 fn main() {
@@ -8,14 +7,14 @@ fn main() {
 
     // ref1.lock().unwrap().push_str("hello ");
     let mut guard1: MutexGuard<String> = ref1.lock().unwrap();
-    let refmut1: &mut String = guard1.deref_mut();
-    String::push_str(refmut1, "hello ");
+    let refmut1: &mut String = &mut *guard1;
+    refmut1.push_str("hello ");
     drop(guard1);
 
     // ref2.lock().unwrap().push_str("world");
     let mut guard2: MutexGuard<String> = ref2.lock().unwrap();
-    let refmut2: &mut String = guard2.deref_mut();
-    String::push_str(refmut2, "world");
+    let refmut2: &mut String = &mut *guard2;
+    refmut2.push_str("world");
     drop(guard2);
 
     println!("{}", my_string.lock().unwrap());
