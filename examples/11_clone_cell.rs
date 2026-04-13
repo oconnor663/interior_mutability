0c59a45c94ae7467292b12a3baf1d7ba48e96e41
Jack O'Connor <oconnor663@gmail.com>
2026-04-13 16:42:16 -0700

    simplify clone cell

--- examples/11_clone_cell.rs
+++ examples/11_clone_cell.rs
@@ -1,56 +1,34 @@
-use std::cell::UnsafeCell;
+use std::cell::Cell;
 use std::rc::Rc;
 
-struct BadCell<T>(UnsafeCell<T>);
-
-impl<T> BadCell<T> {
-    fn new(value: T) -> Self {
-        BadCell(UnsafeCell::new(value))
-    }
-
-    fn set(&self, value: T) {
-        unsafe {
-            *self.0.get() = value;
-        }
-    }
+// XXX: This is unsound.
+fn clone_cell<T: Clone>(cell: &Cell<T>) -> Cell<T> {
+    unsafe { Cell::new((*cell.as_ptr()).clone()) }
 }
 
-impl<T: Clone> Clone for BadCell<T> {
-    fn clone(&self) -> Self {
-        unsafe { BadCell::new((*self.0.get()).clone()) }
-    }
-}
-
+#[derive(Default)]
 struct EvilCloner {
     data: Option<&'static u64>,
-    friends: Vec<Rc<BadCell<EvilCloner>>>,
+    friends: Vec<Rc<Cell<EvilCloner>>>,
 }
 
 impl Clone for EvilCloner {
     fn clone(&self) -> Self {
-        let my_data: Option<&&u64> = match &self.data {
-            Some(ptr) => Some(ptr),
-            None => None,
-        };
-        if let Some(friend) = self.friends.first() {
-            friend.set(EvilCloner {
-                data: None,
-                friends: Vec::new(),
-            });
-        }
-        println!("my data: {}", **my_data.unwrap());
-        unreachable!()
+        // Take a pointer *into* my data for some reason.
+        let data_ptr_ptr: &&u64 = self.data.as_ref().unwrap();
+        // I'm feeling evil today! Clear one of my friends.
+        self.friends.first().unwrap().set(EvilCloner::default());
+        // Use that pointer...SEGFAULT!
+        println!("my data: {}", data_ptr_ptr);
+        todo!()
     }
 }
 
 fn main() {
-    let cell = Rc::new(BadCell::new(EvilCloner {
-        data: Some(&42),
-        friends: Vec::new(),
-    }));
+    let cell = Rc::new(Cell::new(EvilCloner::default()));
     cell.set(EvilCloner {
         data: Some(&42),
         friends: vec![Rc::clone(&cell)],
     });
-    _ = BadCell::clone(&cell);
+    _ = clone_cell(&cell);
 }
