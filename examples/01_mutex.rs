8ad483ebecac8b854cfc9f651dade29dac34b1ac
Jack O'Connor <oconnor663@gmail.com>
2026-04-13 08:09:39 -0700

    factor out functions in 01

--- examples/01_mutex.rs
+++ examples/01_mutex.rs
@@ -2,14 +2,18 @@ use std::sync::Mutex;
 
 static MY_STRING: Mutex<String> = Mutex::new(String::new());
 
+fn thread1() {
+    MY_STRING.lock().unwrap().push_str("hello ");
+}
+
+fn thread2() {
+    MY_STRING.lock().unwrap().push_str("world ");
+}
+
 fn main() {
-    let t1 = std::thread::spawn(|| {
-        MY_STRING.lock().unwrap().push_str("hello ");
-    });
-    let t2 = std::thread::spawn(|| {
-        MY_STRING.lock().unwrap().push_str("world ");
-    });
-    t1.join().unwrap();
-    t2.join().unwrap();
+    let join1 = std::thread::spawn(thread1);
+    let join2 = std::thread::spawn(thread2);
+    join1.join().unwrap();
+    join2.join().unwrap();
     println!("{}", MY_STRING.lock().unwrap());
 }
