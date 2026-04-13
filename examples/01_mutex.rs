6ec3411282058183894488ca92aeefa486bd7a7a
Jack O'Connor <oconnor663@gmail.com>
2026-04-13 12:47:26 -0700

    make them both bg threads

--- examples/01_mutex.rs
+++ examples/01_mutex.rs
@@ -4,16 +4,22 @@ static MY_STRING1: Mutex<String> = Mutex::new(String::new());
 
 fn main() {
     let my_string2 = Arc::new(Mutex::new(String::new()));
-    let joiner = {
+    let joiner1 = {
+        let my_string2 = Arc::clone(&my_string2);
+        std::thread::spawn(move || {
+            MY_STRING1.lock().unwrap().push_str("hello ");
+            my_string2.lock().unwrap().push_str("goodnight ");
+        })
+    };
+    let joiner2 = {
         let my_string2 = Arc::clone(&my_string2);
         std::thread::spawn(move || {
             MY_STRING1.lock().unwrap().push_str("world ");
             my_string2.lock().unwrap().push_str("moon ");
         })
     };
-    MY_STRING1.lock().unwrap().push_str("hello ");
-    my_string2.lock().unwrap().push_str("goodnight ");
-    joiner.join().unwrap();
+    joiner1.join().unwrap();
+    joiner2.join().unwrap();
     println!("{}", MY_STRING1.lock().unwrap());
     println!("{}", my_string2.lock().unwrap());
 }
